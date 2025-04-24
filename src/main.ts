ddc54021bf5df8b51ae3ce01b484ab5e299e44ed
Arslan Ramay <arslanramay104@gmail.com>
2025-04-24 22:57:38 +0400

    Updated App Configs for DB

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-console.log('✅ Loaded ENV:', process.env.APP_PORT);
-console.log('✅ PGHOST =', process.env.DB_HOST);
+// console.log('✅ Loaded ENV:', process.env.APP_PORT);
 console.log('✅ APP_PORT =', process.env.APP_PORT);
+console.log('✅ PGHOST =', process.env.PGHOST);
+console.log('✅ PGDATABASE =', process.env.PGDATABASE);
+console.log('✅ DB_DATABASE =', process.env.DB_DATABASE);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
